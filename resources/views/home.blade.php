a24ca9549cb0f5b372624c8e53b517625b2f741e
ConnorCurry <connor4424@gmail.com>
2025-11-28 20:17:13 -0500

    Added initial homepage content

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,37 @@
 <x-layout>
     <div class="relative">
-        <img class="max-h-175 w-full object-cover object-[0_60%] -mt-20 -z-1 relative" src={{ asset('images/splash-img.webp') }}>
+        <img class="max-h-175 min-h-80 w-full object-cover object-center md:object-[0_60%] -mt-5 md:-mt-20 -z-1 relative" src={{ asset('images/splash-img.webp') }}>
         <div class="absolute top-0 z-2 left-0 w-full h-full bg-black/50 flex flex-col justify-center align-center">
-            <div class="mx-auto max-w-7xl w-full">
-                <h1 class="text-5xl font-bold mb-4">
+            <div class="mx-auto max-w-7xl w-full p-4">
+                <h1 class="text-3xl md:text-5xl font-bold mb-4">
                     Connor Curry
                 </h1>
-                <h2 class="text-2xl inline-block">
+                <h2 class="text-xl md:text-2xl inline-block">
                     <hr class="mb-4">
-                    Web Developer, Programmer, Musician
+                    Web Developer,<br class="md:hidden"> Programmer, Musician
                 </h2>
             </div>
         </div>
     </div>
+    <section>
+        <div class="max-w-7xl mx-auto px-4 py-12 flex flex-wrap gap-8 lg:flex-nowrap">
+            <div class="w-full lg:w-1/2 flex flex-col justify-center">
+                <h1 class="text-3xl mb-4">About Me</h1>
+                <p class="mb-4">An Ithaca College graduate, programmer, web designer, musician, snowboarder and more.</p>
+                <p class="mb-4">
+                    Currently employed at Evolution Marketing as a Senior Frontend Web Developer, I help business both small and large build marketing and SEO friendly websites broaden their reach. 
+                    I manage a team of frontend developers in the maintenance of 600+ active sites built on the <a target="_blank" href="https://cmsmax.com" class="text-indigo-400 cursor-pointer hover:underline hover:text-primary transition">CMS Max</a> platform.
+                </p>
+                <p class="mb-4">
+                    Outside of my work at Evolution Marketing, I enjoy working on side projects in several frameworks, languages and environments. I also spend plenty of time playing guitar and singing with friends and family,
+                    running, snowboarding and playing games with friends. 
+                </p>
+                <a class="text-indigo-400 text-lg hover:translate-x-1 hover:text-primary transition inline-block w-fit" href="/about">Learn More About Me &#8594</a>
+            </div>
+            
+            <div class="w-full md:w-1/2">
+                <img class="rounded-lg w-full" src={{ asset('images/graduation.webp') }}>
+            </div>
+        </div>
+    </section>
 </x-layout>
--- resources/views/livewire/partials/header.blade.php
+++ resources/views/livewire/partials/header.blade.php
@@ -1,6 +1,6 @@
 <div class="w-full relative z-5">
     <div class="w-full bg-primary flex justify-between relative">
-        <a href="/"><h1 class="text-2xl md:text-3xl font-bold text-white pt-2 px-3 md:px-4 md:pt-4">Connor Curry</h1></a>
+        <a href="/" class="hover:translate-x-1 transition"><h1 class="text-2xl md:text-3xl font-bold text-white pt-2 px-3 md:px-4 md:pt-4">Connor Curry</h1></a>
         <nav class="hidden md:flex gap-5 bottom-0 absolute left-1/2 -translate-x-1/2 text-lg">
             <a class="hover:text-white/80 hover:-translate-y-1 transition-all" href="/about">About Me</a>
             <a class="hover:text-white/80 hover:-translate-y-1 transition-all" href="/projects">Projects</a>

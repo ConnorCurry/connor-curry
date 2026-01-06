141be749e30db3f4c172c3422e97747ad7cd3f08
ConnorCurry <connor4424@gmail.com>
2026-01-05 21:37:51 -0500

    Updated panel navigation

--- app/Filament/Resources/Projects/ProjectResource.php
+++ app/Filament/Resources/Projects/ProjectResource.php
@@ -24,7 +24,7 @@ class ProjectResource extends Resource
 
     protected static ?string $recordTitleAttribute = 'Project';
 
-    protected static ?int $navigationSort = 2;
+    protected static ?int $navigationSort = 1;
 
     public static function form(Schema $schema): Schema
     {

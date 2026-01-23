1fedb8c66275a7e8fafab145dd7efa95896dbf7b
ConnorCurry <connor4424@gmail.com>
2026-01-22 20:40:18 -0500

    Added 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,6 @@
+<x-layouts.app>
+    <div class="w-full h-full flex flex-col justify-center items-center py-24">
+        <h2 class="font-bold text-9xl mb-8">404</h2>
+        <h3 class="font-bold text-3xl">Page not found<h3>
+    </div>
+</x-layouts.app>

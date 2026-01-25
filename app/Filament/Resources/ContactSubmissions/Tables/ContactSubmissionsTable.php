68710d85c3107d62e0db906ac62da14dbc73e1cd
ConnorCurry <connor4424@gmail.com>
2026-01-25 15:17:56 -0500

    Added soft delete filter to table for contact submissions

--- app/Filament/Resources/ContactSubmissions/Tables/ContactSubmissionsTable.php
+++ app/Filament/Resources/ContactSubmissions/Tables/ContactSubmissionsTable.php
@@ -5,7 +5,7 @@ namespace App\Filament\Resources\ContactSubmissions\Tables;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteAction;
 use Filament\Actions\DeleteBulkAction;
-use Filament\Actions\EditAction;
+use Filament\Tables\Filters\TrashedFilter;
 use Filament\Actions\ViewAction;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
@@ -34,7 +34,7 @@ class ContactSubmissionsTable
                     ->toggleable(isToggledHiddenByDefault: true),
             ])
             ->filters([
-                //
+                TrashedFilter::make(),
             ])
             ->recordActions([
                 ViewAction::make(),

f94354246c512fce29f3fcd8a94f666597a6e129
ConnorCurry <connor4424@gmail.com>
2026-04-08 01:25:35 -0400

    Updated homepage

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -23,25 +23,25 @@
         <div class="max-w-7xl mx-auto px-3 py-12 flex flex-wrap gap-8 lg:flex-nowrap">
             <div class="w-full lg:w-1/2 flex flex-col justify-center">
                 <h1 class="text-3xl mb-4">About Me</h1>
-                <p class="mb-4">An Ithaca College graduate, programmer, web designer, musician, snowboarder and more.
+                <p class="mb-4">An Ithaca College graduate, programmer, web developer/designer, musician, snowboarder and more.
                 </p>
                 <p class="mb-4">
                     Currently employed at
                     <a target="_blank" href="https://evolutionmarketing.com"
                         class="text-indigo-400 cursor-pointer hover:-translate-y-0.5 hover:text-primary transition inline-block">Evolution
                         Marketing</a>
-                    as a Senior Frontend Web Developer, I help business both small and large build marketing and SEO
-                    friendly websites broaden their reach.
+                    as a Senior Frontend Web Developer, I help businesses both small and large build marketing and SEO
+                    friendly websites to broaden their reach.
                     I manage a team of frontend developers in the maintenance of 600+ active sites built on the <a
                         target="_blank" href="https://cmsmax.com"
                         class="text-indigo-400 cursor-pointer hover:-translate-y-0.5 hover:text-primary transition inline-block">CMS Max</a>
                     platform.
                 </p>
                 <p class="mb-4">
-                    Outside of my work at Evolution Marketing, I enjoy working on side projects in several frameworks,
-                    languages and environments. I also spend plenty of time playing guitar and singing with friends and
+                    Outside of Evolution Marketing, I enjoy working on side projects in several frameworks,
+                    languages and environments. I also spend my free time making music with friends and
                     family,
-                    running, snowboarding and playing games with friends.
+                    running, snowboarding and playing games.
                 </p>
                 <a class="text-indigo-400 text-lg hover:translate-x-1 hover:text-primary transition inline-block w-fit"
                     href="/about">Learn More About Me &#8594</a>
